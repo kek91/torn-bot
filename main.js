c08c80778b707e7a7ba8279462dfac9ab368a62b
Kim Eirik Kvassheim <kim.eirik.kvassheim@otrum.com>
2023-07-26 18:16:44 +0200

    Read env config from netlify

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
 
+let token;
+
+if (process.env.TOKEN) {
+    token = process.env.TOKEN;
+}
+else if(netlifyConfig.build.environment.TOKEN) {
+    token = netlifyConfig.build.environment.TOKEN;
+}
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.login(process.env.TOKEN);
+client.login(token);

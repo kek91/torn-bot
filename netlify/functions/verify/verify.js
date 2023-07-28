579c194696b5c06d6788a55b9f5fe80735a32752
Kim Eirik Kvassheim <kim.eirik.kvassheim@otrum.com>
2023-07-28 23:29:19 +0200

    torn verify function WIP

--- netlify/functions/verify/verify.js
+++ netlify/functions/verify/verify.js
@@ -1,49 +1,48 @@
 const fetch = require('node-fetch')
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const {Client, Events, GatewayIntentBits} = require('discord.js');
 require('dotenv').config()
 
-let token;
-
-if (process.env.TOKEN) {
-  token = process.env.TOKEN;
-}
-else if(netlifyConfig.build.environment.TOKEN) {
-  token = netlifyConfig.build.environment.TOKEN;
-}
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, c => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.login(token);
-
-
 const handler = async function () {
-  try {
-    const response = await fetch('https://icanhazdadjoke.com', {
-      headers: { Accept: 'application/json' },
-    })
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
-    }
-    const data = await response.json()
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ msg: data.joke }),
+    let token;
+
+    if (process.env.TOKEN) {
+        token = process.env.TOKEN;
+    } else if (netlifyConfig.build.environment.TOKEN) {
+        token = netlifyConfig.build.environment.TOKEN;
     }
-  } catch (error) {
-    // output to netlify function log
-    console.log(error)
-    return {
-      statusCode: 500,
-      // Could be a custom message or object i.e. JSON.stringify(err)
-      body: JSON.stringify({ msg: error.message }),
+
+    const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+    client.once(Events.ClientReady, c => {
+        console.log(`Ready! Logged in as ${c.user.tag}`);
+    });
+
+    client.login(token);
+
+    try {
+        const response = await fetch('https://icanhazdadjoke.com', {
+            headers: {Accept: 'application/json'},
+        })
+        if (!response.ok) {
+            // NOT res.status >= 200 && res.status < 300
+            return {statusCode: response.status, body: response.statusText}
+        }
+        const data = await response.json()
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({msg: data.joke}),
+        }
+    } catch (error) {
+        // output to netlify function log
+        console.log(error)
+        return {
+            statusCode: 500,
+            // Could be a custom message or object i.e. JSON.stringify(err)
+            body: JSON.stringify({msg: error.message}),
+        }
     }
-  }
 }
 
-module.exports = { handler }
+module.exports = {handler}

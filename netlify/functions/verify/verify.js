3ae7c488fee39c6797de124d4b333a9b97556c98
Kim Eirik Kvassheim <kim.eirik.kvassheim@otrum.com>
2023-07-28 23:24:17 +0200

    test netlify functions

---
+++ netlify/functions/verify/verify.js
@@ -0,0 +1,49 @@
+const fetch = require('node-fetch')
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
+
+let token;
+
+if (process.env.TOKEN) {
+  token = process.env.TOKEN;
+}
+else if(netlifyConfig.build.environment.TOKEN) {
+  token = netlifyConfig.build.environment.TOKEN;
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
+
+
+const handler = async function () {
+  try {
+    const response = await fetch('https://icanhazdadjoke.com', {
+      headers: { Accept: 'application/json' },
+    })
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: data.joke }),
+    }
+  } catch (error) {
+    // output to netlify function log
+    console.log(error)
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    }
+  }
+}
+
+module.exports = { handler }

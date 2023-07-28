ebe36f869030305e23e53f0b2a6e29b33d074c66
Kim Eirik Kvassheim <kim.eirik.kvassheim@otrum.com>
2023-07-29 00:41:15 +0200

    add node http server

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const http = require("http");
+
 require('dotenv').config()
+
+const host = 'localhost';
+const port = 8000;
 
 let token;
 
@@ -10,6 +15,7 @@ else if(netlifyConfig.build.environment.TOKEN) {
     token = netlifyConfig.build.environment.TOKEN;
 }
 
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
@@ -17,3 +23,28 @@ client.once(Events.ClientReady, c => {
 });
 
 client.login(token);
+
+
+const requestListener = function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/test":
+            res.writeHead(200);
+            res.end(`{"message": "Hello world! This is your friendly bot speaking."}`);
+            break
+        case "/ping":
+            res.writeHead(200);
+            res.end(`{"message": "pong"}`);
+            break;
+        default:
+            res.writeHead(404);
+            res.end(`{"error": "404 Not Found"}`);
+            break;
+    }
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+

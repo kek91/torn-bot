c318fb4a47e96de98732d7231a593c63a2021b82
Kim Eirik Kvassheim <kim.eirik.kvassheim@otrum.com>
2023-07-30 01:29:54 +0200

    auth to torn api and set nick

--- main.js
+++ main.js
@@ -1,62 +1,121 @@
-const { Client, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
+const {Client, Events, GatewayIntentBits, REST, Routes} = require('discord.js');
 const http = require("http");
 
 require('dotenv').config()
 
-const host = 'localhost';
-const port = 8000;
+/**
+ * Authenticate discordjs bot
+ */
 
 let token;
-
 if (process.env.TOKEN) {
     token = process.env.TOKEN;
-}
-else if(netlifyConfig.build.environment.TOKEN) {
+} else if (netlifyConfig.build.environment.TOKEN) {
     token = netlifyConfig.build.environment.TOKEN;
 }
 
+let apikey;
+if (process.env.APIKEY) {
+    apikey = process.env.APIKEY;
+} else if (netlifyConfig.build.environment.APIKEY) {
+    apikey = netlifyConfig.build.environment.APIKEY;
+}
 
+let clientId;
+if (process.env.CLIENTID) {
+    clientId = process.env.CLIENTID;
+} else if (netlifyConfig.build.environment.CLIENTID) {
+    clientId = netlifyConfig.build.environment.CLIENTID;
+}
+
+/**
+ * Setup bot commands
+ */
 
 const commands = [
     {
         name: 'ping',
         description: 'Replies with pong',
     },
+    {
+        name: 'verify',
+        description: 'Verifies your Torn account'
+    }
 ];
 
-const rest = new REST({ version: '10' }).setToken(token);
-
+const rest = new REST({version: '10'}).setToken(token);
 
 try {
-    console.log('started refreshing application (/) commands');
-
-    rest.put(Routes.applicationCommands("1133748577622052874"), {body: commands}).then( () => {
-        console.log('okkkk');
+    console.log('Refreshing application commands');
+    rest.put(Routes.applicationCommands(clientId), {body: commands}).then(() => {
+        console.log('Successfully reloaded application commands!');
     });
-
-    console.log('successfully reloaded application (/) commands');
 } catch (e) {
-    console.log(`Error: ${e}`);
+    console.log(`Error - could not set application commands: ${e}`);
 }
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
 // const channel = await client.channels.cache.get("1133680913398628362");
+
+client.login(token);
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');client.login(token);
-  }
+    console.log(`Received "${interaction.commandName}" interaction`);
+    // console.log(interaction);
+
+    if (interaction.commandName === 'ping') {
+        console.log("Received ping interaction");
+        await interaction.reply('Pong!');
+    }
+
+    else if(interaction.commandName === 'verify') {
+        try {
+            let userIdDiscord = interaction.user.id;
+            console.log(`Verifying discord userid ${userIdDiscord} to Torn API...`);
+
+            const response = await fetch(`https://api.torn.com/user/${userIdDiscord}?selections=discord,basic&comment=tornportal-discord&key=${apikey}`);
+            const data = await response.json();
+
+            if (!response.ok || data.hasOwnProperty('error')) {
+                if (data.hasOwnProperty('error')) {
+                    throw `API Error: ${data.error.error}`;
+                }
+                throw `API Error: Unknown`;
+            }
+
+            if (data.hasOwnProperty('discord') && data.hasOwnProperty('name')) {
+                let userIdTorn = data.player_id;
+                let usernameTorn = data.name;
+                let nickname = `${usernameTorn} [${userIdTorn}]`;
+                console.log(`Verified as Torn user: ${nickname}`);
+                await interaction.reply(`Verified as Torn user: ${nickname}`);
+                // const member = await interaction.options.getMember('user')
+                const member = await interaction.guild.members.fetch(interaction.user.id)
+                await member.setNickname(nickname);
+            }
+            else {
+                console.log(`Could not verify Torn user.`);
+                await interaction.reply(`Could not verify Torn user.`);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
 });
 
 
-const requestListener = function(req, res) {
+/**
+ * node http server
+ */
+
+const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
     switch (req.url) {
         case "/test":
@@ -74,8 +133,9 @@ const requestListener = function(req, res) {
     }
 }
 
+const host = 'localhost';
+const port = 8000;
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
-
